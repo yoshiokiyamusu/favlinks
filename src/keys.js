da1e4dfe1a92aa36733d69c12e655de8f61e1654
yoshiokiyamusu <46406167+yoshiokiyamusu@users.noreply.github.com>
2019-07-29 13:55:07 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'database_links'
+        host: 'fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'cfvc0xg9mms2s789',
+        password: 'zcyp2fux4q42n6rm',
+        database: 'm6ul1nyhger85ik5'
     }
 
 };
